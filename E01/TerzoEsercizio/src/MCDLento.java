9544ee33c737156332d920a237aca476f1354f16
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-04-02 15:25:50 +0200

    E01 - Terminato il terzo esercizio (MCD con diverse implementazioni a confronto)

---
+++ E01/TerzoEsercizio/src/MCDLento.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class MCDLento {
+    public static void main(String[] args) {
+        long a, b;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserire i due numeri oggetto del calcolo del MCD.");
+        System.out.print("Primo numero: ");
+        a = scanner.nextLong();
+        System.out.print("Secondo numero: ");
+        b = scanner.nextLong();
+
+        System.out.println("Il massimo comun divisore tra i due numeri risulta (metodo lento): " + mcdLento(a,b));
+    }
+
+    static long mcdLento(long a, long b){
+        if(b == 0){
+            return a;
+        }
+        if(b > 0 && a >= b){
+            return mcdLento(a-b,b);
+        }
+        else{
+            return mcdLento(b,a);
+        }
+    }
+}
