50883cb4141c158b831dcc2c57caf34fee74b6ef
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-04-02 13:20:28 +0200

    E01 - Creazione progetto PrimoEsercizio

---
+++ E01/PrimoEsercizio/src/Fibonacci.java
@@ -0,0 +1,10 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        //TODO: svolgere l'esercizio
+    }
+
+    static int fib(int n){
+        //placeholder return (temp)
+        return 0;
+    }
+}
