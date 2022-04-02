f4eafb4e650f5e17830085ea4102bb386cbc259a
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-04-02 15:06:39 +0200

    E01 - Completato il primo esercizio (Fibonacci)

--- E01/PrimoEsercizio/src/Fibonacci.java
+++ E01/PrimoEsercizio/src/Fibonacci.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
-        //TODO: svolgere l'esercizio
+        int x = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inserire un numero (posizione nella sequenza di Fibonacci): ");
+        x = scanner.nextInt();
+        System.out.println("Il valore nella " + x + "-esima posizione della sequenza di Fibonacci risulta: " + fib(x));
     }
 
-    static int fib(int n){
-        //placeholder return (temp)
-        return 0;
+    static int fib(int x){
+        if(x == 0){
+            return 0;
+        }
+        if(x == 1 || x == 2){
+            return 1;
+        }
+        else{
+            return fib(x-1) + fib(x-2);
+        }
     }
 }
