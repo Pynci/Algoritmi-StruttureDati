d12231ce499c4eace320490242a105dc3c1b3f64
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-04-05 20:20:40 +0200

    E02 - Terminato il sesto esercizio

--- E02/SestoEsercizio/src/ContaSequenzeBinarie.java
+++ E02/SestoEsercizio/src/ContaSequenzeBinarie.java
@@ -1,10 +1,25 @@
 public class ContaSequenzeBinarie {
     public static void main(String[] args) {
-        int[] array;
-        //da fare
+        int[] array = {1,1,1,0,1,1,1,0,1};
+        System.out.println(count1001(array,0,array.length-1));
     }
 
     static int count1001(int[] a, int left, int right){
-        return 0;
+        if(right - left < 3){
+            return 0;
+        }
+        int middle = (right + left) / 2;
+        int res1 = count1001(a,left, middle);
+        int res2 = count1001(a,middle+1, right);
+        int result = res1 + res2;
+        boolean firstCheck = a[middle-1] == 1 && a[middle] == 0 && a[middle+1] == 0 && a[middle+2] == 1;
+        boolean secondCheck = a[middle-2] == 1 && a[middle-1] == 0 && a[middle] == 0 && a[middle+1] == 1;
+        boolean thirdCheck = a[middle] == 1 && a[middle+1] == 0 && a[middle+2] == 0 && a[middle+3] == 1;
+
+        if(firstCheck || secondCheck || thirdCheck){
+            result++;
+        }
+
+        return result;
     }
 }
