124a94998f0ed1db5591652efda1f8bc85311f09
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-04-05 16:42:45 +0200

    E02 - Creazione progetto del sesto esercizio

---
+++ E02/SestoEsercizio/src/ContaSequenzeBinarie.java
@@ -0,0 +1,10 @@
+public class ContaSequenzeBinarie {
+    public static void main(String[] args) {
+        int[] array;
+        //da fare
+    }
+
+    static int count1001(int[] a, int left, int right){
+        return 0;
+    }
+}
