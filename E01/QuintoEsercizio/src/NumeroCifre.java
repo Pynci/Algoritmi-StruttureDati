b86f95d4b3e2fa1eace203da6d45567552aee83a
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-04-06 20:23:43 +0200

    E01 - Impostato il quinto esercizio

---
+++ E01/QuintoEsercizio/src/NumeroCifre.java
@@ -0,0 +1,20 @@
+public class NumeroCifre {
+    public static void main(String[] args) {
+
+    }
+
+    static int count(int value){
+
+        if(value < 0){
+            return 0;
+        }
+        else if(value < 10){
+            return 1;
+        }
+        else{
+            //continua qui
+        }
+
+        return 0;
+    }
+}
