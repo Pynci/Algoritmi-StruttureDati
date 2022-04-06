ba77595320433c4360a5a838bb2dc6cdfc477f50
Luca Pinciroli <luca.pinciroli02@gmail.com>
2022-04-06 21:06:23 +0200

    E01 - Terminato il quinto esercizio

--- E01/QuintoEsercizio/src/NumeroCifre.java
+++ E01/QuintoEsercizio/src/NumeroCifre.java
@@ -1,6 +1,6 @@
 public class NumeroCifre {
     public static void main(String[] args) {
-
+        System.out.println(count(998337));
     }
 
     static int count(int value){
@@ -12,9 +12,7 @@ public class NumeroCifre {
             return 1;
         }
         else{
-            //continua qui
+            return 1 + count(value/10);
         }
-
-        return 0;
     }
 }
